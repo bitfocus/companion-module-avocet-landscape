ee21cbdc41e8c7bcc9bc054d9f0a2c2944fcfda1
avocet-oli <oliver.hill@avocet.tv>
2023-08-16 11:50:07 +0100

    Initial release
    First set of changes, developed and tested locally and implemented in its entirety

--- actions.js
+++ actions.js
@@ -1,3 +1,17 @@
+const fs = require( 'fs' );
+
+// The "num" parameter was originally used to connect the key press to the button name 
+// but was replaced by the button parameter which provides the actual name.
+// The declaration was removed but this was kept as an example
+// {
+// 	id: 'num',
+// 	type: 'number',
+// 	label: 'Button Id',
+// 	default: 5,
+// 	min: 0,
+// 	max: 100,
+// },
+
 module.exports = function (self) {
 	self.setActionDefinitions({
 		sample_action: {
@@ -14,6 +28,48 @@ module.exports = function (self) {
 			],
 			callback: async (event) => {
 				console.log('Hello world!', event.options.num)
+			},
+		},
+		press_action: {
+			name: 'ButtonPressed',
+			options: [
+				{
+					id: 'button',
+					type: 'textinput',
+					label: 'Button Name',
+					default: 'not set',
+				},
+			],
+			callback: async (event) => {
+				const details = { id:event.options.num, button:event.options.button, action: "pressed", time:Date.now() };
+				const json_text = JSON.stringify( details );
+				console.log('Button event', json_text );
+
+				fs.writeFile( '/home/companion/python/queue/action.json', json_text, (err) =>
+				{
+					if ( err ) console.log( 'Error writing to buttons queue' );
+				});
+			},
+		},
+		release_action: {
+			name: 'ButtonReleased',
+			options: [
+				{
+					id: 'button',
+					type: 'textinput',
+					label: 'Button Name',
+					default: 'not set',
+				},
+			],
+			callback: async (event) => {
+				const details = { id:event.options.num, button:event.options.button, action: "released", time:Date.now() };
+				const json_text = JSON.stringify( details );
+				console.log('Button event', json_text );
+
+				fs.writeFile( '/home/companion/python/queue/action.json', json_text, (err) =>
+				{
+					if ( err ) console.log( 'Error writing to buttons queue' );
+				});
 			},
 		},
 	})

f03f7bcfe46bb30614c608dcfef9569fdd7cc15e
Julian Waller <git@julusian.co.uk>
2023-08-30 21:22:11 +0100

    tidying

--- actions.js
+++ actions.js
@@ -14,22 +14,22 @@ const fs = require( 'fs' );
 
 module.exports = function (self) {
 	self.setActionDefinitions({
-		sample_action: {
-			name: 'My First Action',
-			options: [
-				{
-					id: 'num',
-					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 100,
-				},
-			],
-			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
-			},
-		},
+		// sample_action: {
+		// 	name: 'My First Action',
+		// 	options: [
+		// 		{
+		// 			id: 'num',
+		// 			type: 'number',
+		// 			label: 'Test',
+		// 			default: 5,
+		// 			min: 0,
+		// 			max: 100,
+		// 		},
+		// 	],
+		// 	callback: async (event) => {
+		// 		console.log('Hello world!', event.options.num)
+		// 	},
+		// },
 		press_action: {
 			name: 'ButtonPressed',
 			options: [
